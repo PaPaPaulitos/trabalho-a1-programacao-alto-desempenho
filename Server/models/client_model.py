16966fdabef1f2ea069edfdbd731a9d0095be8f6
PAULO  RICARDO MESQUITA ROSA SANTOS <12683759770@veigadealmeida.edu.br>
2024-04-08 19:39:52 -0300

    (feat): Adding Client Model and Cripto Service

---
+++ Server/models/client_model.py
@@ -0,0 +1,4 @@
+class Client:
+    def __init__(self,ipv4,cripto:str):
+        self.ipv4 = ipv4
+        self.cripto = cripto
---
+++ Server/services/get_cripto.py
@@ -0,0 +1,7 @@
+import random
+
+class Cripto:
+
+    def get_cripto():
+        return random.randint(1,1000)
+    
